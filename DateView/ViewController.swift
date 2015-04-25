e3855db62bd160c6e36b3356665e8f02de394fae
Stephen Maitland <s.maitland@me.com>
2015-04-25 19:14:12 +0100

    Initial code

--- DateView/ViewController.swift
+++ DateView/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func showDay(sender: AnyObject) {
+        // grab the chosen date
+        var chosenDate = self.datePicker.date
+        
+        // Create an NSDateFormatter
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        
+        // Grab the day and create a message
+        let day = formatter.stringFromDate(chosenDate)
+        let result = "That was a \(day)"
+        
+        // Create alert controller
+        let myAlert = UIAlertController(title: result, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        myAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        
+        // Show the alert
+        self.presentViewController(myAlert, animated: true, completion: nil )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
