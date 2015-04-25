fafdcd58d6939197d4aa66f9c1db11a09466b0e1
Stephen Maitland <s.maitland@me.com>
2015-04-25 18:41:55 +0100

    Initial Commit

---
+++ DateView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateView
+//
+//  Created by Stephen on 25/04/2015.
+//  Copyright (c) 2015 vantech.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
